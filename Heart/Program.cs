b40a36da6ef03d2421dd6fb4faaacbaecb524421
xarlein <alexvolkovdd@gmail.com>
2023-02-11 17:40:16 +0400

    bugfix #0

---
+++ Heart/Program.cs
@@ -0,0 +1,27 @@
+﻿public sealed class Program
+{
+	private static void Main()
+	{
+		Console.WriteLine(
+		    """
+
+
+
+		                      ######       #######
+		                  ###       ### ###       ###
+		                ##             #             ##
+		              #                                 #
+		              #        H A L F - L I F E        #
+		              #                                 # 
+		              = ##                           ## =
+		              = == ##                     ## == =
+		                == == ##               ## == ==
+		                   == == ##         ## == ==
+		                      == == ####### == ==
+		                         == ======= ==
+		                            =======
+		""");
+
+		Console.ReadKey();
+	}
+}
